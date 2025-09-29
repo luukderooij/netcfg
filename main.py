3c34e9c4761f4e6d96b822e27a33e3f8bf0ee43e
Luuk de Rooij <luukderooij@gmail.com>
2025-09-29 16:07:48 +0200

    refactor: add logging configuration to main execution block

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from netcfg.gui.main import launch_gui
+import logging
+
 
 if __name__ == "__main__":
+    
+    logging.basicConfig(
+        level=logging.INFO,  # of DEBUG
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
     launch_gui()
